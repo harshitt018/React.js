11435edd86a24a7f3736acb69bbd46a6c2971ff7
Harshit Jaiswal <bscit.harshitjaiswal@gmail.com>
2024-11-23 19:52:28 +0530

    Create users.js

---
+++ src/backend/users.js
@@ -0,0 +1,22 @@
+[
+  {
+    "email": "h@gmail.com",
+    "password": "$2a$10$GnqAGJtOSsGJrQU1eVihyevO4TV9CondImKNVv.5/fUSPsEGiWQJy"
+  },
+  {
+    "email": "ankit@gmail.com",
+    "password": "$2a$10$cVm6KN4I5JbJDKE99K38Te0FQv1.Ibgvbw.G0NJUFbfXoD1h6kO86"
+  },
+  {
+    "email": "hj@gmail.com",
+    "password": "$2a$10$iFUijk0qLa3UAePHFaULl.kA6sho8ZhTINRxWF8qvAdNulfQs0lpu"
+  },
+  {
+    "email": "harshitjaiswal@gmail.com",
+    "password": "$2a$10$379LCgaNTDx.yu3ZUCGyAe73EnVxJHypyu./k34ZUd.t0beDhRyKa"
+  },
+  {
+    "email": "react@gmail.com",
+    "password": "$2a$10$BCNXe0QxTKXbKcAVV7GwbeUE0Sq4fCrcDuORmqHibsMtbqYHeZTl6"
+  }
+]
